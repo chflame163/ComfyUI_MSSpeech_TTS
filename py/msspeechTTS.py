f42235905f2eee70bcd56118428b2a73c35efb60
chflame163 <chflame@163.com>
2024-01-08 15:40:50 +0800

    Adding the voice list can be customized by modifying voicelist.ini

--- py/msspeechTTS.py
+++ py/msspeechTTS.py
@@ -1,14 +1,33 @@
 import asyncio
 import datetime
-
 import edge_tts
 import numpy as np
 import folder_paths
 import os
 
-async def gen_tts(_text,_voice,_rate,filename):
-    tts = edge_tts.Communicate(text = _text, voice = _voice, rate = _rate)
+async def gen_tts(_text, _voice, _rate, filename):
+    tts = edge_tts.Communicate(text=_text, voice=_voice, rate=_rate)
     await tts.save(filename)
+
+INIFILE = os.path.join(os.path.dirname(os.path.dirname(os.path.normpath(__file__))), "voicelist.ini")
+voice_dict = {}
+try:
+    print('# MSSpeech: Load voice setting -> ' + INIFILE)
+    with open(INIFILE, 'r') as f:
+        ini = f.readlines()
+
+        for line in ini:
+            if not line.startswith('#') and len(line) > 1:
+                api_name = line[: line.find('(')].rstrip()
+                gender = line[line.find('(') + 1:-2]
+                lang = api_name[:api_name.rfind('-')]
+                name = api_name[api_name.rfind('-') + 1:api_name.find('Neural')]
+                display_item = lang + ':' + name + '(' + gender + ')'
+                voice_dict.update({display_item: api_name})
+        print('# MSSpeech: ' + str(len(voice_dict)) + ' voice load successfully.')
+except Exception as e:
+    print('# MSSpeech: ERROR -> ' + repr(e))
+
 
 class Text2AudioEdgeTts:
     def __init__(self):
@@ -18,14 +37,8 @@ class Text2AudioEdgeTts:
 
     @classmethod
     def INPUT_TYPES(cls):
-        VOICES=[
-'zh-CN-XiaoxiaoNeural','zh-CN-XiaoyiNeural','zh-CN-YunjianNeural','zh-CN-YunxiNeural','zh-CN-YunxiaNeural',
-'zh-CN-YunyangNeural','zh-CN-liaoning-XiaobeiNeural','zh-CN-shaanxi-XiaoniNeural','zh-HK-HiuGaaiNeural',
-'zh-HK-HiuMaanNeural','zh-HK-WanLungNeural','zh-TW-HsiaoChenNeural','zh-TW-HsiaoYuNeural','zh-TW-YunJheNeural',
-'en-US-AnaNeural','en-US-AriaNeural','en-US-ChristopherNeural','en-US-EricNeural','en-US-GuyNeural',
-'en-US-JennyNeural','en-US-MichelleNeural','en-US-RogerNeural','en-US-SteffanNeural',
-'en-GB-LibbyNeural','en-GB-MaisieNeural','en-GB-RyanNeural','en-GB-SoniaNeural','en-GB-ThomasNeural'
-                ]
+
+        VOICES =  list(voice_dict.keys())
         return {
             "required": {
                 "voice": (VOICES, ),
@@ -42,15 +55,16 @@ class Text2AudioEdgeTts:
     CATEGORY = "MicrosoftSpeech_TTS"
 
     def text_2_audio(self,voice,filename_prefix,text,rate):
+        voice_name = voice_dict[voice]
         full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir)
         _datetime = datetime.datetime.now().strftime("%Y%m%d")
         _datetime = _datetime + datetime.datetime.now().strftime("%H%M%S%f")
-        file = f"{filename}_{_datetime}_{voice}.mp3"
+        file = f"{filename}_{_datetime}_{voice_name}.mp3"
         audio_path=os.path.join(full_output_folder, file)
         _rate = str(rate) + "%" if rate < 0 else "+" + str(rate) + "%"
-        print(f"MicrosoftSpeech TTS: Generating voice files, voice=‘{voice}’, rate={rate}, audiofile_path='{audio_path}, 'text='{text}'")
+        print(f"MicrosoftSpeech TTS: Generating voice files, voice=‘{voice_name}’, rate={rate}, audiofile_path='{audio_path}, 'text='{text}'")
 
-        asyncio.run(gen_tts(text,voice,_rate,audio_path))
+        asyncio.run(gen_tts(text,voice_name,_rate,audio_path))
 
         return {"ui": {"text": "Audio file："+os.path.join(full_output_folder, file),
         'audios':[{'filename':file,'type':'output','subfolder':'audio'}]}, "result": (audio_path, )}
